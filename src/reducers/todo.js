88fbeb1a362dff01b2df3a17cad5ed9130efdb4c
Cem Ozturan <cozturan@findmypast.com>
2017-08-21 13:57:32 +0100

    Minor refactoring, making action names constants

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -7,11 +7,16 @@ const initialState = {
   currentTodo: ''
 };
 
+const CURRENT_UPDATE = 'CURRENT_UPDATE';
+const TODO_ADD = 'TODO_ADD';
+
+export const updateCurrent = (val) => ({type: CURRENT_UPDATE, payload: val});
+
 export default function Todo(state = initialState, action) {
   switch(action.type) {
-    case "TODO_ADD":
+    case TODO_ADD:
       return {...state, todos: state.todos.concat(action.payload)};
-    case "CURRENT_UPDATE":
+    case CURRENT_UPDATE:
       return {...state, currentTodo: action.payload};
     default:
       return state;
